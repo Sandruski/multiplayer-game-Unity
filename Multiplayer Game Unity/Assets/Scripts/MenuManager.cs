323b952381fdcbb4eff80184431849d9e930c5ef
DatBeQuiet <kayter72@gmail.com>
2019-12-15 21:41:39 +0100

    Main Menu

--- Multiplayer Game Unity/Assets/Scripts/MenuManager.cs
+++ Multiplayer Game Unity/Assets/Scripts/MenuManager.cs
@@ -3,8 +3,25 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    public void OnHostButton()
+    public GameObject mainSection;
+    public GameObject onlineSection;
+    public GameObject lanSection;
+    public void OnBackButton()
     {
-        SceneManager.LoadScene("MainScene");
+        mainSection.SetActive(true);
+        lanSection.SetActive(false);
+        onlineSection.SetActive(false);
+    }
+
+    public void OnLanButton()
+    {
+        mainSection.SetActive(false);
+        lanSection.SetActive(true);
+    }
+
+    public void OnOnlineButton()
+    {
+        mainSection.SetActive(false);
+        onlineSection.SetActive(true);
     }
 }
