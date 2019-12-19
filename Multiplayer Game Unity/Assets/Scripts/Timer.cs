543f5f650d53a60604921efd348485364d4501cf
Sandra Alvarez <sandruskiag@gmail.com>
2019-12-19 22:03:26 +0100

    Timer

---
+++ Multiplayer Game Unity/Assets/Scripts/Timer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class Timer : NetworkBehaviour
+{
+    [SyncVar(hook = "OnTimerUpdated")]
+    private float timer = 120.0f;
+
+    private Text text;
+
+    void Start()
+    {
+        text = GetComponent<Text>();   
+    }
+
+    void Update()
+    {
+        if (!isServer)
+        {
+            return;
+        }
+
+        timer -= Time.deltaTime;
+    }
+
+    void OnTimerUpdated(float timer)
+    {
+        this.timer = timer;
+
+        int time = (int)this.timer;
+        text.text = time.ToString();
+    }
+}
